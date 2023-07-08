c9cdb34288a836674f520ca1ef9d86efaa0cc326
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-07-08 10:59:06 +0530

    Create 05_Ass.js

---
+++ dsa/05_Ass.js
@@ -0,0 +1,32 @@
+function merge(nums1, m, nums2, n) {
+  let i = m - 1; // Pointer for nums1
+  let j = n - 1; // Pointer for nums2
+  let k = m + n - 1; // Pointer for the merged array (nums1)
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+
+  // Copy any remaining elements from nums2 to nums1
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+}
+
+// Example usage:
+const nums1 = [1, 2, 3, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6];
+const n = 3;
+
+merge(nums1, m, nums2, n);
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
